b38d781625e0b848c5645601f503a7cdb434536e
donghyun <gorusair@gmail.com>
2025-08-14 16:45:14 +0900

    새로운 Viewcontroller 추가

---
+++ Swift_Playground/gdhViewController.swift
@@ -0,0 +1,29 @@
+//
+//  gdhViewController.swift
+//  Swift_Playground
+//
+//  Created by DONGHYUN on 8/14/25.
+//
+
+import UIKit
+
+class gdhViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
